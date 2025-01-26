20c53ff3e40924d6655cd5f1c17d8a93e010ae62
maria <mariataktasheva@gmail.com>
2025-01-26 13:29:36 -0800

    add profile picture

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -26,7 +26,7 @@ export const aboutMe: AboutMe = {
     "I'm a first-year <a href='https://theialab.ca'>PhD student</a> working in the intersection of fields of computer vision and graphics. My current research focuses on better geometry in Gaussian Splatting — creating a better 3D representation of the world.",
   email: "maria_taktasheva@sfu.ca",
   imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "/images/profile.jpeg",
   googleScholarUrl: "https://scholar.google.com/citations?user=CcUdBLcAAAAJ&hl=en",
   githubUsername: "mtaktash",
   linkedinUsername: "maria-taktasheva",
